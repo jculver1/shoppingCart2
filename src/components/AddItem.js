2e3cf229c8163b09e00bbdb0edb9fb16984f18e5
jculver1 <jculver1900@gmail.com>
2019-03-22 13:54:44 -0600

    added extra padding to add items

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -2,20 +2,21 @@ import React from 'react'
 
 const AddItems = (props) => {
     return (
-        <div>
-            <form>
-                <div className = "form-group">
-                    <label htmlFor="exampleFormControlSelect1">Select Item</label>
-                    <select onChange ={props.selectItem} className="form-control" id="exampleFormControlSelect1">
-                    {props.product.map(item => <option key={item.id} value = {item.name} price = {item.price}> {item.name} ${(item.priceInCents / 100).toFixed(2)} </option>)}
-                    </select>
-                </div>
-                <div className = "form-group">
-                    <label htmlFor="exampleFormControlInput1">Quantity:</label>
-                    <input onChange = {props.quantity} type="number" className="form-control" id="quantity" data-bind="value:replyNumber"></input>
-                </div>
-                <button onClick = {props.addItem} type="submit" className="btn btn-primary">Add Item</button>
-            </form>
+        <div class ="row">
+            <div class="col-1 ml-5"></div>
+                <form>
+                    <div className = "form-group">
+                        <label htmlFor="exampleFormControlSelect1">Select Item</label>
+                        <select onChange ={props.selectItem} className="form-control" id="exampleFormControlSelect1">
+                        {props.product.map(item => <option key={item.id} value = {item.name} price = {item.price}> {item.name} ${(item.priceInCents / 100).toFixed(2)} </option>)}
+                        </select>
+                    </div>
+                    <div className = "form-group">
+                        <label htmlFor="exampleFormControlInput1">Quantity:</label>
+                        <input onChange = {props.quantity} type="number" className="form-control" id="quantity" data-bind="value:replyNumber"></input>
+                    </div>
+                    <button onClick = {props.addItem} type="submit" className="btn btn-primary">Add Item</button>
+                </form>
         </div>
     )
    
